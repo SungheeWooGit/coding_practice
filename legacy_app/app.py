54f7cc99978f23b99589a81212d1c7d16cb07da5
Ubuntu <ubuntu@ip-172-31-26-166.us-west-1.compute.internal>
2025-07-16 08:31:13 -0700

    add CORS

--- legacy_app/app.py
+++ legacy_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -8,13 +8,24 @@ TASKS = [
     {"id": 2, "title": "Update docs", "credit_card": "4111-1111-1111-1111"}
 ]
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    return response
+
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
+    if request.method == 'OPTIONS':
+        return make_response('', 204)  # Preflight response
     print("DEBUG: Returning sensitive data")  # Logs secrets (insecure)
     return jsonify(TASKS)
 
 @app.route("/tasks", methods=["POST"])
 def add_task():
+    if request.method == 'OPTIONS':
+        return make_response('', 204)  # Preflight response
     task = request.json
     task["id"] = len(TASKS) + 1
     TASKS.append(task)
@@ -23,6 +34,8 @@ def add_task():
 
 @app.route("/tasks/<int:task_id>", methods=["DELETE"])
 def delete_task(task_id):
+    if request.method == 'OPTIONS':
+        return make_response('', 204)  # Preflight response
     global TASKS
     TASKS = [t for t in TASKS if t["id"] != task_id]
     return jsonify({"result": "deleted"})
